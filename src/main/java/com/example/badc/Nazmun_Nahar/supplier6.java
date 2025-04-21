0454bcc78ee26455a51def90a00d4ba3b6a617fd
shamme <shammetaffannum6@gmail.com>
2025-04-21 19:51:41 +0600

    controller supplier6.fxml

--- src/main/java/com/example/badc/Nazmun_Nahar/supplier6.java
+++ src/main/java/com/example/badc/Nazmun_Nahar/supplier6.java
@@ -1,19 +1,21 @@
+package com.example.badc.Nazmun_Nahar;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class supplier6 {
-
-    @FXML
+public class supplier6
+{
+    @javafx.fxml.FXML
     private TextField ProductIdTextArea;
-
-    @FXML
+    @javafx.fxml.FXML
     private TextArea successfullMassageTextArea;
 
-    @FXML
-    void DeleteProductOnAction(ActionEvent event) {
-
+    @javafx.fxml.FXML
+    public void initialize() {
     }
 
+    @javafx.fxml.FXML
+    public void DeleteProductOnAction(ActionEvent actionEvent) {
+    }
 }
