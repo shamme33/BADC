9ff97aa516982cb9a13ce9b0398b3676e1d7d758
shamme <shammetaffannum6@gmail.com>
2025-04-21 19:42:24 +0600

    fxml for supplier5

--- src/main/java/com/example/badc/Nazmun_Nahar/supplier5.java
+++ src/main/java/com/example/badc/Nazmun_Nahar/supplier5.java
@@ -1,29 +1,27 @@
+package com.example.badc.Nazmun_Nahar;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class supplier5 {
-
-    @FXML
+public class supplier5
+{
+    @javafx.fxml.FXML
+    private TextField ProductIdTextField;
+    @javafx.fxml.FXML
+    private TextField PriceTextField;
+    @javafx.fxml.FXML
     private TextField InitialStockTextField;
-
-    @FXML
+    @javafx.fxml.FXML
     private TextField NameTextField;
-
-    @FXML
+    @javafx.fxml.FXML
     private Label NewProductAddedLabel;
 
-    @FXML
-    private TextField PriceTextField;
-
-    @FXML
-    private TextField ProductIdTextField;
-
-    @FXML
-    void AddButtonONAction(ActionEvent event) {
-
+    @javafx.fxml.FXML
+    public void initialize() {
     }
 
+    @javafx.fxml.FXML
+    public void AddButtonONAction(ActionEvent actionEvent) {
+    }
 }
-
