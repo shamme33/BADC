a56ae194aff530d847eb32071f50bd7b768e1c0e
Ashrafy169 <2130184@iub.edu.bd>
2025-04-21 22:34:42 +0600

    user 2(Admin) goal2 fx id fixed and controller class has been updated.checked if null or not with array list,

---
+++ BADC/src/main/java/com/example/badc/Hannan_Ashrafy/User2goal2.java
@@ -0,0 +1,34 @@
+package com.example.badc.Hannan_Ashrafy;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class User2goal2
+{
+    @javafx.fxml.FXML
+    private TableColumn <Admin,String>farmerTbleColumnNID;
+    @javafx.fxml.FXML
+    private TableColumn<Admin,String> farmerAgeTableColumn;
+    @javafx.fxml.FXML
+    private TextField farmersearchTextFiled;
+    @javafx.fxml.FXML
+    private TableColumn <Admin,String>farmerTablecolumn;
+    @javafx.fxml.FXML
+    private Text farmerSearchTexttable;
+    @javafx.fxml.FXML
+    private TableView<Admin> farmerTable;
+    @javafx.fxml.FXML
+    private Button saveButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void saveInfoOnClick(ActionEvent actionEvent) {
+    }
+}
